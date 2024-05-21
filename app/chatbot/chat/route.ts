8da8b589ac57e4f0b6eef537866d99d556429acb
Jakob Ossmann <Jakobossmann@gmail.com>
2024-05-21 14:14:34 +1000

    apikey new

--- app/chatbot/chat/route.ts
+++ app/chatbot/chat/route.ts
@@ -1,9 +1,9 @@
 import OpenAI from "openai";
 
-// Assign API key to variable
-const apiKey = process.env.OPEN_AI_KEY;
-// Initialise OpenAI API
-const openai = new OpenAI({ apiKey: apiKey });
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY
+  , dangerouslyAllowBrowser: true
+});
 
 
 export async function POST(req: Request) {
